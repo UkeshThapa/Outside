c1af10be4a015eb5fd93890a4fe47df656a6e5bf
UkeshThapa <yukeshthapa8@gmail.com>
2023-01-26 15:15:07 +0545

    assignment4

--- Internship_assignments/JavaScript/assignment4/index.js
+++ Internship_assignments/JavaScript/assignment4/index.js
@@ -1,9 +1,14 @@
 const widthCointainer =  500;
 const heightCointainer =  500;
 
+function createElement(element, className = null, id = null, targetName){
+    var element = document.createElement(element);
+    element.className= className
+    document.querySelector(targetId).appendChild(element);
+}
 
 function main(){
-    console.log('Hello');
+    
 }
 
 main();
